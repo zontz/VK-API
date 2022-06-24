eff30841b6eb7d2fecd7298b3b0eab00488fb888
Shlyakhovenko Vladislav <homyak603@gmail.com>
2022-06-24 12:52:00 +0300

    build(app): create app

---
+++ VK-API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK-API
+//
+//  Created by Владислав Шляховенко on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
